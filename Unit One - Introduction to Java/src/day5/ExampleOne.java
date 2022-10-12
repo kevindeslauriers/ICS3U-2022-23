77921c9c086f814f99b1416173112a20f18de636
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-10-12 10:26:22 -0400

    CoffeeCup Class Using Objects

---
+++ Unit One - Introduction to Java/src/day5/ExampleOne.java
@@ -0,0 +1,19 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ExampleOne {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("Please enter the radius: ");
+      double radius = in.nextDouble();
+
+      double area = Math.PI * radius * radius;
+
+      System.out.println("The area of the circle of radius " + radius + " is: " + area);
+
+      in.close();
+
+   }
+}
