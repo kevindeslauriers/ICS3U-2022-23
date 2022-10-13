f0b460783693d7a43d5dc1f4b6b54e544e54a6c7
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-10-13 11:41:19 -0400

    Bayview Glen Pools

---
+++ Unit Two - Using Objects/src/assignment/BayviewGlenPools.java
@@ -0,0 +1,15 @@
+package assignment;
+
+import java.util.Scanner;
+
+/**
+ * BayviewGlenPools
+ */
+public class BayviewGlenPools {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      int x = in.nextInt();
+   }
+
+}
